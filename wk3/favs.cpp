671e608414718ae4a0bb3ac65383c321b2789c60
Orange Julius <rehenson7@gmail.com>
2026-05-14 13:00:34 -0500

    Add files before final

--- wk3/favs.cpp
+++ wk3/favs.cpp
@@ -31,15 +31,55 @@ int main() {
         }
         else if(input == "add") {
             // TODO: ask for a game name and push_back into games
+            cout << "What is the game name? ";
+            string gameName;
+            getline(cin, gameName);
+            games.push_back(gameName);
         }
         else if(input == "edit") {
-            // TODO: show games, ask which to edit, replace it
+            sort(games.begin(), games.end());
+            for(int i = 0; i < games.size(); i++) {
+                cout << i + 1 << ". " << games[i] << "\n";
+            }
+            cout << "What number would you like to edit? \n";
+            int choice;
+            cin >> choice;
+            cin.ignore();
+            if(choice >= 1 && choice <= games.size()) {
+                cout << "What is the new name? \n";
+                string newName;
+                getline(cin, newName);
+                games[choice - 1] = newName;
+                cout << "Game updated.\n";
+            }
+            else {
+                cout << "That number is not on the list.\n";
+            }
         }
         else if(input == "remove") {
-            // TODO: show games, ask which to remove, erase it
+            //TODO: remove games, asks for which number, removes
+            sort(games.begin(), games.end());
+            for(int i = 0; i < games.size(); i++) {
+                cout << i + 1 << ". " << games[i] << "\n";
+            }
+            cout << "What number would you like to remove? \n";
+            int choice;
+            cin >> choice;
+            cin.ignore();
+            if(choice >= 1 && choice <= games.size()) {
+                games.erase(games.begin() + choice - 1);
+                cout << "Game removed.\n";
+            }
+            else {
+                cout << "That number is not on the list.\n";
+            }
         }
         else if(input == "show") {
             // TODO: sort games and print each one
+            sort(games.begin(), games.end());
+            for(int i = 0; i < games.size(); i++) {
+                cout << i + 1 << ". " << games[i] << "\n";
+            }
         }
         else {
             cout << "I don't recognize that input.\n";
